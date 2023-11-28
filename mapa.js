95b677177103b3ffb42cb4f35b1f5509ea4f6108
Paula Castellanos <pcastellanoso@unal.edu.co>
2023-11-28 18:16:50 -0500

    versión 1.0 proyecto GInt

---
+++ mapa.js
@@ -0,0 +1,44 @@
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el primer punto.
+function Punto1() {
+	document.getElementById("ubicacion-info").innerHTML = '<br/><h2 class="subtitulo1">Plaza Mayor de Villa de Leyva</h2> <br/>'+
+	'<img class="imagen" src="Imagenes/Uno.jpg" alt="punto uno"><br/>'+
+	'La Plaza Mayor de Villa de Leyva, en Colombia, es una de las plazas más grandes de América Latina y es conocida por su arquitectura colonial bien conservada. Si estás planeando visitar o hacer actividades en la Plaza Mayor de Villa de Leyva, aquí tienes algunas sugerencias:<br>'+
+	'<strong>1. Explorar la Plaza a Pie:</strong> Disfruta de un paseo tranquilo por la plaza, admirando la arquitectura colonial y la atmósfera histórica.<br>'+
+	'<strong>2. Participar en Eventos Locales:</strong> Investiga si hay eventos o festivales locales programados durante tu visita. La plaza a menudo es el escenario de celebraciones culturales y eventos especiales.<br>'+
+	'<strong>3. Recorrido Nocturno:</strong> Disfruta de la Plaza Mayor iluminada por la noche. Algunos lugares pueden tener música en vivo o actividades nocturnas.';
+	document.getElementById("ubicacion-info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el segundo punto.
+function Punto2() {
+	document.getElementById("ubicacion-info").innerHTML = '<br/><h2 class="subtitulo1">Terminal de transporte de Villa de Leyva</h2> <br/>'+
+	'<img class="imagen" src="Imagenes/Dos.jpg" alt="punto dos"><br/>'+
+	'El terminal se encuenta ubicado en la Calle 8 número 8 - 50 y cuenta con salas de espera para los pasajeros, parqueaderos, bahías de abordaje, restaurantes y Cafeterías, guarda equipajes, capilla, sistema de seguridad. Wi Fi, baños, servicio de Taxi y cuenta con acceso para personas discapacitadas.';
+	document.getElementById("ubicacion-info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el tercer punto.
+function Punto3() {
+	document.getElementById("ubicacion-info").innerHTML = '<br/><h2 class="subtitulo1">Plaza de mercado de Villa de Leyva</h2> <br/>'+
+	'<img class="imagen" src="Imagenes/Tres.jpg" alt="punto tres"><br/>'+
+	'La Plaza de Mercado de Villa de Leyva es un lugar vibrante y colorido donde los habitantes locales y visitantes pueden comprar una variedad de productos frescos, alimentos, artesanías y más. Aquí tienes algunas recomendaciones para aprovechar tu visita a la Plaza de Mercado de Villa de Leyva:<br>'+
+	'<strong>1. Productos Locales:</strong> Explora los puestos que ofrecen productos locales, como frutas frescas, verduras, hierbas, y otros alimentos frescos cultivados en la región. Es una excelente oportunidad para probar productos auténticos de Villa de Leyva.<br>'+
+	'<strong>2. Productos Orgánicos:</strong> Si te interesa la alimentación saludable, busca puestos que ofrezcan productos orgánicos o productos de agricultura sostenible.';
+	document.getElementById("ubicacion-info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el cuarto punto.
+function Punto4() {
+	document.getElementById("ubicacion-info").innerHTML = '<br/><h2 class="subtitulo1">Parque Antonio Nariño de Villa de Leyva</h2> <br/>'+
+	'<img class="imagen" src="Imagenes/Cuatro.jpg" alt="punto cuatro"><br/>'+
+	'Este parque se encuentra adornado con una impresionante estatua del General Antonio Nariño y bancos estratégicamente dispuestos, invita a quienes lo visitan a sentarse, contemplar y sumergirse en la pintoresca atmósfera circundante. Los árboles proporcionan sombra y frescura, creando un refugio perfecto para evadir el calor y el ajetreo cotidiano. Se conoce como un punto de encuentro donde se respira la autenticidad de Villa de Leyva, ofreciendo a las personas la oportunidad de disfrutar de momentos serenos y conectarse con la comunidad y la rica historia que alberga este lugar.';
+	document.getElementById("ubicacion-info").scrollIntoView();
+}
+
+//Función para cambiar el párrafo posterior al mapa interactivo para contener información sobre el quinto punto.
+function Punto5() {
+	document.getElementById("ubicacion-info").innerHTML = '<br/><h2 class="subtitulo1">Hospital San Francisco de Villa de Leyva</h2> <br/>'+
+	'<img class="imagen" src="Imagenes/Cinco.jpg" alt="punto cinco"><br/>'+
+	'No está de más tener presente la ubicación del Hospital recientemente construido en caso de cualquier imprevisto, o para simplemente realizarte un chequeo general ;).';
+	document.getElementById("ubicacion-info").scrollIntoView();
+}
